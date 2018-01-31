b137b86598822b6fcf0a20f1582a11b95582f93a
Nathan Moore <ntmoore@gmail.com>
2018-01-30 21:52:15 -0600

    Add files via upload

---
+++ 01-31-print-function/01-31-print-function.ino
@@ -0,0 +1,25 @@
+/* 
+Using a fucntion to same in the number of lines in the function
+*/
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int i = 0;
+  while (i < 10) {
+    delay(1000);
+    i = i + 1;
+    // using a function to run a couple of print statements
+    print_out_int(i);
+  }
+  Serial.println();
+
+}
+
+void print_out_int(int j) {
+  Serial.print(j);
+  Serial.print(" ");
+  return;
+}
+
