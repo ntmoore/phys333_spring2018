7f8ebff4e4e92b64a79bc934104c7e5a17b766de
Nathan Moore <ntmoore@gmail.com>
2018-02-01 13:08:50 -0600

    SOS function examples
    Note how much cleaner/easier to audit code with functions can be!  Example from Nathan Anderson's HW

---
+++ 02-02-SOS-w-functions/02-02-SOS-w-functions.ino
@@ -0,0 +1,58 @@
+/* This is Nathan Anderson's solution to the SOS code.
+   Note the nice use of functions to compartmentalize (ie, not screw up) the
+   pulse timing.
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); //Have the built-in LED and pin 13 as output
+}
+
+void loop() {
+  SOS();    //Blinks SOS in morse code
+}
+
+/*
+    SOS()
+    Function that runs an SOS pattern in morse code on pin 13 of the Arduino
+*/
+void SOS() {
+  for (int s = 1; s <= 3; ++s) {
+    dot();
+  }
+  delay(100);
+
+  for (int o = 1; o <= 3; ++o) {
+    dash();
+  }
+  delay(100);
+
+  for (int s = 1; s <= 3; ++s) {
+    dot();
+  }
+
+  delay(1500);
+}
+
+/*
+    dot()
+    Function that writes one morse code dot to pin 13, complements dash()
+*/
+void dot() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);   //One unit time = 250 ms
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+}
+
+/*
+    dash()
+    Function that writes one morse code dash to pin 13, complements dot()
+*/
+void dash() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(750);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+}
+
+
---
+++ 02-02-sos-no-functions/02-02-sos-no-functions.ino
@@ -0,0 +1,40 @@
+/* This is Nathan Anderson's solution to the SOS code, BUT all of the functions
+    have been "inlined".  Note that this would be comparitively difficult
+    to debug/audit.
+  Nathan Moore, Feb 1
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); //Have the built-in LED and pin 13 as output
+}
+
+void loop() {
+  //SOS();    //Blinks SOS in morse code
+  for (int s = 1; s <= 3; ++s) {
+    //dot();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);   //One unit time = 250 ms
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+  delay(100);
+
+  for (int o = 1; o <= 3; ++o) {
+    //dash();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(750);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+  delay(100);
+
+  for (int s = 1; s <= 3; ++s) {
+    //dot();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);   //One unit time = 250 ms
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+
+  delay(1500);
+}
