9c609436ea17c1c359b209edc715ce7fffa5e721
Nathan Moore <ntmoore@gmail.com>
2018-02-15 23:16:23 -0600

    Add files via upload

---
+++ 02-16-digitalRead-1/02-16-digitalRead-1.ino
@@ -0,0 +1,24 @@
+/* intro to digitalRead()
+    turn a light on if a button is pushed
+*/
+const int buttonPin = 2;
+const int ledPin = 3;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  /* note the compact (lazy code).  digitalRead() returns an int so I can
+      use it inside a conditional block.  This works but can be hard to read
+      if done too much
+  */
+  if (digitalRead(buttonPin)) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(2000);
+
+}
