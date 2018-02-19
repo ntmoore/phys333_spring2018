6e1ee4c028c45585fe3efb7a5926238012c02b5a
Nathan Moore <ntmoore@gmail.com>
2018-02-19 08:20:53 -0600

    This example is for lab 4

---
+++ 02-19-Lab-4-pwm_read/02-19-Lab-4-pwm_read.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, INPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // system PWM output on 3, duty = 48/255
+  analogWrite(3, 48);
+  // run a wire from pin 3 to pin 13
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  delay(500);
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  delay(500);
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  Serial.println(digitalRead(13));
+  delay(2000);
+}
